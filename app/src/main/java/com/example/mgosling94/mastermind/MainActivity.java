18964130aaa9c5b3fa4912084629b7af85ac2eca
Michael Gosling <w0246627@nscc.ca>
2018-11-22 14:59:53 +0000

    built function to fill hashmap with colors, built UI for user input. Still need to code logic for selecting colors and submitting guess.

--- app/src/main/java/com/example/mgosling94/mastermind/MainActivity.java
+++ app/src/main/java/com/example/mgosling94/mastermind/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.mgosling94.mastermind;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    private HashMap<String, Integer> colorIdMap = new HashMap<String, Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Fills given hashmap with possible colors IDs
+     *
+     * @param hashMap HashMap to fill
+     */
+    protected void fillMapWithColors(HashMap<String, Integer> hashMap) {
+        hashMap.put("Color1", R.color.color1);
+        hashMap.put("Color2", R.color.color2);
+        hashMap.put("Color3", R.color.color3);
+        hashMap.put("Color4", R.color.color4);
+        hashMap.put("Color5", R.color.color5);
+        hashMap.put("Color6", R.color.color6);
+    }
+
+    /**
+     * Update the graphical representation of the board
+     */
+    protected void updateBoard() {
+
+    }
+
+    /**
+     * Take the current colors of the user input UI and pass it to business class
+     */
+    protected void submitGuess() {
+
+    }
+
 }
