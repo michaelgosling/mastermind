3cc7040246b4fffbe41dd062f90192705a09f106
Michael Gosling <w0246627@nscc.ca>
2018-12-06 09:38:07 -0400

    Merged CodePeg and KeyPeg into Peg with a flag to identify if it's a keypeg, and an enum to identify the color

---
+++ app/src/main/java/com/example/mgosling94/mastermind/Peg.java
@@ -0,0 +1,24 @@
+package com.example.mgosling94.mastermind;
+
+public class Peg {
+
+    enum PegColor {
+        White, Black, Color1, Color2, Color3, Color4, Color5, Color6
+    }
+
+    /* Properties */
+    private PegColor color;
+    private boolean isKey;
+
+    /* Accessors & Mutators */
+    public PegColor getColor() {
+        return color;
+    }
+
+    /* Constructor */
+    public Peg(PegColor color, boolean isKey) {
+        this.color = color;
+        this.isKey = isKey;
+    }
+
+}
