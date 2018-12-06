9a8edeb79746f0f814229eb70c09ff6847022f46
Michael Gosling <w0246627@nscc.ca>
2018-12-06 09:23:26 -0400

    Created CodePeg and KeyPeg classes

---
+++ app/src/main/java/com/example/mgosling94/mastermind/CodePeg.java
@@ -0,0 +1,17 @@
+package com.example.mgosling94.mastermind;
+
+public class CodePeg {
+    /* Properties */
+    private int color;
+
+    /* Accessors & Mutators */
+    public int getColor() {
+        return color;
+    }
+
+    /* Constructor */
+    public CodePeg(int color) {
+        this.color = color;
+    }
+
+}
---
+++ app/src/main/java/com/example/mgosling94/mastermind/KeyPeg.java
@@ -0,0 +1,16 @@
+package com.example.mgosling94.mastermind;
+
+public class KeyPeg {
+    /* Properties */
+    private int color;
+
+    /* Accessors & Mutators */
+    public int getColor() {
+        return color;
+    }
+
+    /* Constructor */
+    public KeyPeg(int color) {
+        this.color = color;
+    }
+}
